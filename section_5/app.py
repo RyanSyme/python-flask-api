63d14b362f3d984c68f238938525389b63f37b4b
RyanSyme <ryu321@hotmail.com>
2022-01-11 18:26:56 +0000

    moved database to sqlite and added requirements.txt

--- section_5/app.py
+++ section_5/app.py
@@ -14,11 +14,11 @@ items = []
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type=float,
-            required=True,
-            help="This field cannot be left blank!"
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
     )
     
     @jwt_required()
