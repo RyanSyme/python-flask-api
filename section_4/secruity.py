23d4b578d9c21b9079b1a8facab217e57d82b606
RyanSyme <ryu321@hotmail.com>
2022-01-11 13:48:44 +0000

    add security.py

---
+++ section_4/secruity.py
@@ -0,0 +1,20 @@
+users = [
+    {
+        "id": 1,
+        "username": "Brad",
+        "password": "password"
+    }
+]
+
+username_mapping = { 'bob' : {
+    "id": 1,
+    "username": "Brad",
+    "password": "password"
+    }
+}
+
+userid_mapping = { 1: {
+    "id": 1,
+    "username": "Brad",
+    "password": "password"
+}}
